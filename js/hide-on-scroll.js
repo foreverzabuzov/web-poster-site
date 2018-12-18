db2e27b036409e073140c2c2eb3c10bf6f1c936f
Denis Z <denzel3330@gmail.com>
2018-12-19 02:57:29 +0300

    some changes

--- js/hide-on-scroll.js
+++ js/hide-on-scroll.js
@@ -11,3 +11,13 @@ $(window).scroll(function(){
     $(".arrow_down").css("opacity", 1 - $(window).scrollTop() / 760);
     $(".arrow_down2").css("opacity", 2 - $(window).scrollTop() / 1000);
   });
+
+
+  var divs = $('.why');
+  $(window).scroll(function(){
+     if($(window).scrollTop()<10){
+           divs.stop(true,true).fadeIn("fast");
+     } else {
+           divs.stop(true,true).fadeOut("fast");
+     }
+  });
